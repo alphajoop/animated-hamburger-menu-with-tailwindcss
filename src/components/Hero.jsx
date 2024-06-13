0edc3900d7084d13a1cc2e6475a0d50427f436c7
Alpha DIOP <alphakhoss@gmail.com>
2024-06-13 12:06:54 +0000

    ✨:: introduce hero feature

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,47 @@
 export default function Hero() {
   return (
-    <section className="mt-10 px-4 md:px-16">
-      <div>
-        <h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
-          vel quis laudantium labore mollitia, quaerat reiciendis corporis? Nam,
-          quo? Quos voluptas harum aperiam voluptatibus consequuntur, veniam
-          quia recusandae. Ad, provident?
-        </h1>
+    <section className="mt-10 px-4 font-inter md:px-16">
+      <div className="mx-auto grid max-w-screen-xl py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
+            Payments tool for software companies
+          </h1>
+          <p className="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
+            From checkout to global sales tax compliance, companies around the
+            world use Flowbite to simplify their payment stack.
+          </p>
+          <a
+            href="#"
+            className="mr-3 inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white transition-colors duration-700 ease-in  hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 focus:ring-offset-0 max-[349px]:mb-4"
+          >
+            Get started
+            <svg
+              className="-mr-1 ml-2 h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 transition-colors duration-700 ease-in hover:bg-gray-100 focus:ring-2 focus:ring-gray-100"
+          >
+            Speak to Sales
+          </a>
+        </div>
+        <div className="hidden lg:col-span-5 lg:mt-0 lg:flex">
+          <img
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+            alt="mockup"
+          />
+        </div>
       </div>
     </section>
   );
